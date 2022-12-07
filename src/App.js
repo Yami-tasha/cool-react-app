46106cba8fbcde3542f09d0e6eba1fd42bf8c2b3
Yami-tasha <113445980+Yami-tasha@users.noreply.github.com>
2022-12-07 15:18:55 +0000

    Added changes required by Netlify requirements

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
         <Weather />
   <footer>
     This project was created by Nataliia Rubtsova and is {" "}
-    <a href="https://github.com/Yami-tasha/cool-react-app" target="_blank">open-sourced on GitHab</a>
+    <a rel="noreferrer" href="https://github.com/Yami-tasha/cool-react-app" target="_blank">open-sourced on GitHab</a>
   </footer>
     </div>
     </div>
