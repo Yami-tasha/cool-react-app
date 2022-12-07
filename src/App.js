654a4feee68df61026622748a89c4fee0b514b4d
Yami-tasha <113445980+Yami-tasha@users.noreply.github.com>
2022-12-07 15:06:57 +0000

    Adede bacik styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div class="container">
-  <h1>Weather</h1>
+        <Weather />
   <footer>
     This project was created by Nataliia Rubtsova and is {" "}
     <a href="https://github.com/Yami-tasha/cool-react-app" target="_blank">open-sourced on GitHab</a>
