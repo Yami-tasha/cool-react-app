a56975e0d530241994ffb978db9b64b4de96c5e8
Yami-tasha <113445980+Yami-tasha@users.noreply.github.com>
2022-12-07 11:24:34 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div class="container">
   <h1>Weather</h1>
   <footer>
     This project was created by Nataliia Rubtsova and is {" "}
     <a href="https://github.com/Yami-tasha/cool-react-app" target="_blank">open-sourced on GitHab</a>
   </footer>
+    </div>
     </div>
   );
 }
