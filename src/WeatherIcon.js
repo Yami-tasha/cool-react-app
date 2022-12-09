f19977a1ae452710a76bd69c3318a5b44f73b85f
Yami-tasha <113445980+Yami-tasha@users.noreply.github.com>
2022-12-09 14:34:13 +0000

    Added WeatherIcon and included axios

---
+++ src/WeatherIcon.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+
+export default function WeatherIcon(props) {
+ 
+  return (
+    <ReactAnimatedWeather
+      icon="CLEAR_DAY"
+      color="#1e1e1e"
+      size={50}
+      animate={true}
+    />
+  );
+}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormatDate from "./FormatDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
     return (
@@ -13,10 +14,7 @@ export default function WeatherInfo(props) {
                 </ul>
                 <div className="row mt-3">
                     <div className="col-6">
-                        <img 
-                        src={props.data.iconUrl}
-                        alt={props.data.description}
-                        />
+                    <WeatherIcon code={props.data.icon} />
                         <span className="temperature">{Math.round(props.data.temperature)}</span>
                         <span className="unit">°C</span>   
                     </div>
